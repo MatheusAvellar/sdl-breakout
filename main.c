9adbfc786a6cbed83ff87b2c8f163f7a7da8659e
PDiasG <pdiasg@gmail.com>
2017-06-10 20:00:09 -0300

    Re-designed menu
    -Added Window icon

--- main.c
+++ main.c
@@ -147,6 +147,9 @@ SDL_Surface* gBRICKSurface = NULL;
 // Current displayed PNG image (racket)
 SDL_Surface* gPLAYERSurface = NULL;
 
+// Icon image
+SDL_Surface* gIcon = NULL;
+
 // Image for buttons
 SDL_Surface* buttonplay = NULL;
 SDL_Surface* buttonoptions = NULL;
@@ -273,25 +276,25 @@ int main(int argc, char* args[]) {
 
 void menu(void) {
 
-    int buttonplay_x = 300;
-    int buttonplay_y = 255;
+    int buttonplay_x = 592;
+    int buttonplay_y = 12;
     int buttonplay_w = 400;
     int buttonplay_h = 50;
 
-    int buttonoptions_x = 325;
-    int buttonoptions_y = 375;
+    int buttonoptions_x = 642;
+    int buttonoptions_y = 66;
     int buttonoptions_w = 350;
     int buttonoptions_h = 50;
 
-    int buttonrankings_x = 350;
-    int buttonrankings_y = 495;
+    int buttonrankings_x = 692;
+    int buttonrankings_y = 121;
     int buttonrankings_w = 300;
     int buttonrankings_h = 50;
 
-    int breakout_x = 100;
-    int breakout_y = 20;
-    int breakout_w = 800;
-    int breakout_h = 160;
+    int breakout_x = 0;
+    int breakout_y = 0;
+    int breakout_w = 1000;
+    int breakout_h = 700;
 
     // Mouse position
     int mouseX, mouseY;
@@ -324,7 +327,7 @@ void menu(void) {
         && mouseY <= buttonplay_y + buttonplay_h) {
 
             SDL_SetColorKey(buttonplay, SDL_FALSE,
-                            SDL_MapRGB(buttonplay->format, 0x99, 0xD9, 0xEA));
+                            SDL_MapRGB(buttonplay->format, 0x70, 0x92, 0xBE));
 
             // Check if buttonplay is pressed
             if (SDL_GetMouseState(NULL, NULL)
@@ -334,7 +337,7 @@ void menu(void) {
             }
         } else {
             SDL_SetColorKey(buttonplay, SDL_TRUE,
-                            SDL_MapRGB(buttonplay->format, 0x99, 0xD9, 0xEA));
+                            SDL_MapRGB(buttonplay->format, 0x70, 0x92, 0xBE));
         }
 
         // Check if mouse is over buttonoptions
@@ -344,7 +347,7 @@ void menu(void) {
         && mouseY <= buttonoptions_y + buttonoptions_h) {
 
             SDL_SetColorKey(buttonoptions, SDL_FALSE,
-                         SDL_MapRGB(buttonoptions->format, 0x99, 0xD9, 0xEA));
+                         SDL_MapRGB(buttonoptions->format, 0x70, 0x92, 0xBE));
 
             // Check if buttonoptions is pressed
             if(SDL_GetMouseState(NULL, NULL)
@@ -354,7 +357,7 @@ void menu(void) {
             }
         } else {
             SDL_SetColorKey(buttonoptions, SDL_TRUE,
-                         SDL_MapRGB(buttonoptions->format, 0x99, 0xD9, 0xEA));
+                         SDL_MapRGB(buttonoptions->format, 0x70, 0x92, 0xBE));
         }
 
         // Check if mouse is over buttonrankings
@@ -364,7 +367,7 @@ void menu(void) {
         && mouseY <= buttonrankings_y + buttonrankings_h) {
 
             SDL_SetColorKey(buttonrankings, SDL_FALSE,
-                        SDL_MapRGB(buttonrankings->format, 0x99, 0xD9, 0xEA));
+                        SDL_MapRGB(buttonrankings->format, 0x70, 0x92, 0xBE));
 
             //C heck if buttonrankings is pressed
             if(SDL_GetMouseState(NULL, NULL)
@@ -374,25 +377,25 @@ void menu(void) {
             }
         } else {
             SDL_SetColorKey(buttonrankings, SDL_TRUE,
-                        SDL_MapRGB(buttonrankings->format, 0x99, 0xD9, 0xEA));
+                        SDL_MapRGB(buttonrankings->format, 0x70, 0x92, 0xBE));
         }
 
         // Fill the surface with #000000 (black)
         SDL_FillRect(gScreenSurface, NULL, SDL_MapRGB(gScreenSurface->format,
                                                     0x00, 0x00, 0x00));
 
-        if(drawOnScreen(buttonplay, 0, 0,
-                         buttonplay_w, buttonplay_h,
-                         buttonplay_x, buttonplay_y) < 0
+        if(drawOnScreen(breakout, 0, 0,
+                         breakout_w, breakout_h,
+                         breakout_x, breakout_y) < 0
         || drawOnScreen(buttonoptions, 0, 0,
                          buttonoptions_w, buttonoptions_h,
                          buttonoptions_x, buttonoptions_y) < 0
         || drawOnScreen(buttonrankings, 0, 0,
                          buttonrankings_w, buttonrankings_h,
                          buttonrankings_x, buttonrankings_y) < 0
-        || drawOnScreen(breakout, 0, 0,
-                         breakout_w, breakout_h,
-                         breakout_x, breakout_y) < 0){
+        || drawOnScreen(buttonplay, 0, 0,
+                        buttonplay_w, buttonplay_h,
+                        buttonplay_x, buttonplay_y) < 0){
             /* TODO: Clean this if condition */
             error(ERR_BLIT);
             quit = true;
@@ -728,7 +731,7 @@ void options(void) {
       && mouseY <= buttonhome_y + buttonhome_h) {
 
           SDL_SetColorKey(buttonhome, SDL_FALSE,
-                          SDL_MapRGB(buttonhome->format, 0x99, 0xD9, 0xEA));
+                          SDL_MapRGB(buttonhome->format, 0x70, 0x92, 0xBE));
 
           // Check if button home is pressed
           if (SDL_GetMouseState(NULL, NULL)
@@ -738,7 +741,7 @@ void options(void) {
           }
       } else {
           SDL_SetColorKey(buttonhome, SDL_TRUE,
-                          SDL_MapRGB(buttonhome->format, 0x99, 0xD9, 0xEA));
+                          SDL_MapRGB(buttonhome->format, 0x70, 0x92, 0xBE));
       }
 
     // Fill the surface with #000000 (black)
@@ -800,7 +803,7 @@ void ranking(void) {
       && mouseY <= buttonhome_y + buttonhome_h) {
 
           SDL_SetColorKey(buttonhome, SDL_FALSE,
-                          SDL_MapRGB(buttonhome->format, 0x99, 0xD9, 0xEA));
+                          SDL_MapRGB(buttonhome->format, 0x70, 0x92, 0xBE));
 
           // Check if button home is pressed
           if (SDL_GetMouseState(NULL, NULL)
@@ -810,7 +813,7 @@ void ranking(void) {
           }
       } else {
           SDL_SetColorKey(buttonhome, SDL_TRUE,
-                          SDL_MapRGB(buttonhome->format, 0x99, 0xD9, 0xEA));
+                          SDL_MapRGB(buttonhome->format, 0x70, 0x92, 0xBE));
       }
 
     // Fill the surface with #000000 (black)
@@ -1124,6 +1127,45 @@ int init(void) {
         return false;
     }
 
+    Uint16 pixels[16*16] = {  // ...or with raw pixel data:
+    0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff,
+    0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff,
+    0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff,
+    0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff,
+    0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff,
+    0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff,
+    0x0fff, 0x0aab, 0x0789, 0x0bcc, 0x0eee, 0x09aa, 0x099a, 0x0ddd,
+    0x0fff, 0x0eee, 0x0899, 0x0fff, 0x0fff, 0x1fff, 0x0dde, 0x0dee,
+    0x0fff, 0xabbc, 0xf779, 0x8cdd, 0x3fff, 0x9bbc, 0xaaab, 0x6fff,
+    0x0fff, 0x3fff, 0xbaab, 0x0fff, 0x0fff, 0x6689, 0x6fff, 0x0dee,
+    0xe678, 0xf134, 0x8abb, 0xf235, 0xf678, 0xf013, 0xf568, 0xf001,
+    0xd889, 0x7abc, 0xf001, 0x0fff, 0x0fff, 0x0bcc, 0x9124, 0x5fff,
+    0xf124, 0xf356, 0x3eee, 0x0fff, 0x7bbc, 0xf124, 0x0789, 0x2fff,
+    0xf002, 0xd789, 0xf024, 0x0fff, 0x0fff, 0x0002, 0x0134, 0xd79a,
+    0x1fff, 0xf023, 0xf000, 0xf124, 0xc99a, 0xf024, 0x0567, 0x0fff,
+    0xf002, 0xe678, 0xf013, 0x0fff, 0x0ddd, 0x0fff, 0x0fff, 0xb689,
+    0x8abb, 0x0fff, 0x0fff, 0xf001, 0xf235, 0xf013, 0x0fff, 0xd789,
+    0xf002, 0x9899, 0xf001, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0xe789,
+    0xf023, 0xf000, 0xf001, 0xe456, 0x8bcc, 0xf013, 0xf002, 0xf012,
+    0x1767, 0x5aaa, 0xf013, 0xf001, 0xf000, 0x0fff, 0x7fff, 0xf124,
+    0x0fff, 0x089a, 0x0578, 0x0fff, 0x089a, 0x0013, 0x0245, 0x0eff,
+    0x0223, 0x0dde, 0x0135, 0x0789, 0x0ddd, 0xbbbc, 0xf346, 0x0467,
+    0x0fff, 0x4eee, 0x3ddd, 0x0edd, 0x0dee, 0x0fff, 0x0fff, 0x0dee,
+    0x0def, 0x08ab, 0x0fff, 0x7fff, 0xfabc, 0xf356, 0x0457, 0x0467,
+    0x0fff, 0x0bcd, 0x4bde, 0x9bcc, 0x8dee, 0x8eff, 0x8fff, 0x9fff,
+    0xadee, 0xeccd, 0xf689, 0xc357, 0x2356, 0x0356, 0x0467, 0x0467,
+    0x0fff, 0x0ccd, 0x0bdd, 0x0cdd, 0x0aaa, 0x2234, 0x4135, 0x4346,
+    0x5356, 0x2246, 0x0346, 0x0356, 0x0467, 0x0356, 0x0467, 0x0467,
+    0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff,
+    0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff,
+    0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff,
+    0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff
+  };
+  gIcon = SDL_CreateRGBSurfaceFrom(pixels,16,16,16,16*2,0x0f00,0x00f0,0x000f,0xf000);
+
+    // The icon is attached to the window pointer
+    SDL_SetWindowIcon(gWindow, gIcon);
+
     // Initialize PNG loading
     int imgFlags = IMG_INIT_PNG;
     if(!(IMG_Init(imgFlags) & imgFlags)) {
@@ -1158,7 +1200,7 @@ int loadMedia(void) {
     || (buttonoptions = loadSurface("./images/optionsbutton.png")) == NULL
     || (buttonrankings = loadSurface("./images/rankingbutton.png")) == NULL
     || (side_bar = loadSurface("./images/side_bar.png")) == NULL
-    || (breakout = loadSurface("./images/breakout.png")) == NULL
+    || (breakout = loadSurface("./images/homeback.png")) == NULL
     || (sound = loadSurface("./images/sound.png")) == NULL
     || (buttonhome = loadSurface("./images/homebutton.png")) == NULL) {
         error(ERR_IMG_LOAD);
